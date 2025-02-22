0a1a7a1ca2313ecf3a74b398b79957acd05984d3
Eleganceeeee <liangzzzj@gmail.com>
2025-02-22 14:24:52 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('yes').addEventListener('click', function() {
-    alert('你答应了！❤️');
-});
-
-document.getElementById('no').addEventListener('click', function() {
-    alert('好吧，不要也没关系。');
-});
