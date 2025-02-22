d0f4d95b21732a9ae6b3011c946c873376801e62
Eleganceeeee <liangzzzj@gmail.com>
2025-02-22 14:14:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-{\rtf1\ansi\ansicpg936\cocoartf2821
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+document.getElementById('yes').addEventListener('click', function() {
+    alert('你答应了！❤️');
+});
 
-\f0\fs24 \cf0 document.getElementById('yes').addEventListener('click', function() \{\
-    alert('\uc0\u20320 \u31572 \u24212 \u20102 \u65281 \u10084 \u65039 ');\
-\});\
-\
-document.getElementById('no').addEventListener('click', function() \{\
-    alert('\uc0\u22909 \u21543 \u65292 \u19981 \u35201 \u20063 \u27809 \u20851 \u31995 \u12290 ');\
-\});}
+document.getElementById('no').addEventListener('click', function() {
+    alert('好吧，不要也没关系。');
+});
